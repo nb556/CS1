bddb07cf74603f2922a41d8dcad11e856831353a
nb556 <nicklas.bouchard@gmail.com>
2022-08-16 11:32:02 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Goodbye world!");
+        int a = 15;
+        int b = 6;
+        System.out.println ("the value of a is " + a);
+        System.out.println (b);
+        System.out.println (a-b);
+        System.out.println (a/b);
+        System.out.println ((3*a) % (3*b));
     }
 }
