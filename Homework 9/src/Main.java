9b8767df2503607bc0d1a79de37703ee7fe2a622
nb556 <nicklas.bouchard@gmail.com>
2022-08-24 15:56:09 +0300

    CS1

--- Homework 9/src/Main.java
+++ Homework 9/src/Main.java
@@ -2,24 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int program = 2;
-        if (program == 1)
-        {
-            int a = 5;
-            int result = 1;
 
-            for (int i = 1; i <= a; i++)
-                result *= 3;
-            System.out.println(result);
-        }
-        else if (program == 2)
-        {
-            int a = 2, b = 5;
-            int result = 1;
-
-            for (int i = 1; i <= a; i++)
-                result *= b;
-            System.out.println(result);
-        }
     }
 }
