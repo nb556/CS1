71bc9236b0077c6c01d586abc2595e3108830770
nb556 <nicklas.bouchard@gmail.com>
2022-08-24 15:35:41 +0300

    CS1

---
+++ Homework 5/src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int A = 101;
+        int B = 49;
+        int result = 0;
+
+        if(A >= 100) {
+            if (B <= 50) {
+                result = 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
