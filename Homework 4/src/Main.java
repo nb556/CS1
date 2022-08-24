ef4016249446654fcfcf0f7906d4208f57aa20a3
nb556 <nicklas.bouchard@gmail.com>
2022-08-24 15:26:13 +0300

    CS1

---
+++ Homework 4/src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int T = 20;
+
+        if (T < 17)
+            System.out.println("hit");
+        else if (T < 21)
+            System.out.println("stay");
+        else
+            System.out.println("bust");
+    }
+}
