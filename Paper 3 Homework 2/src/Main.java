6aff1ca9c3dc831ca3ef455e3214b0c924ef9e86
nb556 <nicklas.bouchard@gmail.com>
2022-08-26 11:45:17 +0300

    CS1

---
+++ Paper 3 Homework 2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean shining = true;
+        int time = 11;
+
+        if (shining == true && time >= 10 && time <= 16)
+            System.out.println("Jane should be using sunscreen.");
+        else
+            System.out.println("Jane doesn't need to use sunscreen.");
+
+
+
+    }
+}
