ca32b5e08e770c67a9153849a8095c952cacab94
nb556 <nicklas.bouchard@gmail.com>
2022-08-26 09:33:32 +0300

    CS1

--- Homework 9/src/Main.java
+++ Homework 9/src/Main.java
@@ -2,6 +2,29 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int t = 0;
+        int bact1;
+        int bact2 = 0;
+        int highest = 0;
+        int highestt = 0;
 
+
+        while ( t < 100)
+        {
+
+            bact1 = t*(t-20)*(t-100)+120000;
+
+            if (bact1 - bact2 > highest) {
+                highest = bact2 - bact1;
+                highestt = t;
+            }
+
+            t++;
+
+            bact2 = (t+1)*((t+1)-20)*((t+1)-100)+120000;
+
+        }
+
+        System.out.println(highestt+1);
     }
 }
