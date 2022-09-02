de6d55a5883015bed04b8a9acc236c3ee33cbb43
nb556 <nicklas.bouchard@gmail.com>
2022-09-02 10:35:44 +0300

    CS1

--- Paper 4 Homework 1/src/Main.java
+++ Paper 4 Homework 1/src/Main.java
@@ -2,14 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        double smallest = 10000;
         double[] elements = {2, 5, -3, 11, 193, -2};
 
         for (int i = 0; i < elements.length; i++) {
-            if (elements[i] < smallest)
-                smallest = elements[i];
+            System.out.println(elements[i]);
         }
 
-        System.out.println(smallest);
+
     }
 }
