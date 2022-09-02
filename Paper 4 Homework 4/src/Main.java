06f93164e57b3f6a0ba4859314734fc373bd8983
nb556 <nicklas.bouchard@gmail.com>
2022-09-02 11:31:15 +0300

    CS1

---
+++ Paper 4 Homework 4/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] original = {5, 4, -7, -2, -1};
+
+        for (int i = original.length - 1; i >= 0; i--)
+        {
+            System.out.println(original[i]);
+        }
+    }
+}
