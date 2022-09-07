ec2ead1518f046cb519db0447aa35c2b3bed02aa
nb556 <nicklas.bouchard@gmail.com>
2022-09-07 14:56:52 +0300

    CS1

--- Paper 4 Homework 5/src/Main.java
+++ Paper 4 Homework 5/src/Main.java
@@ -3,13 +3,13 @@ public class Main
     public static void main(String[] args)
     {
         int[] original = {-3, 2, 6, -11};
-        int sum = original[1];
+        int sum = original[0];
 
         System.out.println(original[0]);
 
         for (int i = 1; i < original.length; i++)
         {
-            sum = sum + original[i -1];
+            sum = sum + original[i];
             System.out.println(sum);
         }
     }
---
+++ Paper 4 Homework 6/src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {4, -3, -5, 9, 1, 11};
+        int[] smaller = new int[0];
+        int[] larger = new int[0];
+        int m = 1;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < m)
+                smaller[i] = arr[i];
+            else if (arr[i] > m)
+            {
+                larger[i] = arr[i];
+            }
+        }
+    }
+}
