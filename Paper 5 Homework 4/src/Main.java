d36c9f71917b676ea9bcb08b951d0fd74d77422a
nb556 <nicklas.bouchard@gmail.com>
2022-09-12 13:53:10 +0300

    CS1

--- Paper 5 Homework 4/src/Main.java
+++ Paper 5 Homework 4/src/Main.java
@@ -2,8 +2,23 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = reverse(a);
 
+        for (int i = 0; i < b.length; i++)
+        {
+            System.out.println(b[i]);
+        }
     }
 
+    static int[] reverse (int[] a)
+    {
+        int[] x = new int[a.length];
 
+        for (int i = 0; i < a.length; i++)
+        {
+            x[i] = a[a.length - 1 - i];
+        }
+        return x;
+    }
 }
